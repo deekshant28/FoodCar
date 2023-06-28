8fbd8a89908ef836e9db74b88b0ef484693dce00
deekshant28 <120581502+deekshant28@users.noreply.github.com>
2023-06-29 01:09:27 +0530

    Update Navbar.js

--- FOODCAR/src/components/Navbar.js
+++ FOODCAR/src/components/Navbar.js
@@ -2,8 +2,7 @@
 
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Badge from "react-bootstrap/Badge";
 import { useCart } from './ContextReducer';
 import Modal from '../Modal';
 import Cart from '../screens/Cart';
@@ -28,7 +27,7 @@ export default function Navbar(props) {
             <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
                 style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic" to="/">FoodCar</Link>
+                    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -43,10 +42,10 @@ export default function Navbar(props) {
                                 </li> : ""}
                         </ul>
                         {(!localStorage.getItem("token")) ?
-                            <form className="d-flex">
+                            <div className="d-flex">
                                 <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
                                 <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
-                            </form> :
+                            </div> :
                             <div>
 
                                 <div className="btn bg-white text-success mx-2 " onClick={loadCart}>
